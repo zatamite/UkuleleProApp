43379f304a24a0658f753ff0c57e45220dcc72d7
Peter Farell <zatamite@users.noreply.github.com>
2026-02-18 08:39:10 -0600

    Initial Commit

---
+++ UkuleleProApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UkuleleProApp
+//
+//  Created by Peter Farell on 2/18/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UkuleleProApp/UkuleleProAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UkuleleProAppApp.swift
+//  UkuleleProApp
+//
+//  Created by Peter Farell on 2/18/26.
+//
+
+import SwiftUI
+
+@main
+struct UkuleleProAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
